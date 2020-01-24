6526e8beccd965915ae11a45943a2340815567cd
unknown <robert.tanksley@gmail.com>
2020-01-23 20:24:43 -0600

    finished all challenges and stretch

--- index.js
+++ index.js
@@ -41,7 +41,20 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name; 
+    this.age = age; 
+    this.stomach = [];
+  }
+  eat(someFood){
+    if (this.stomach.length < 10) return this.stomach.push(someFood);
+    }
+  poop(){
+    this.stomach = [];  
+  }  
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
 }
 
 /*
@@ -59,7 +72,24 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.tank = 0;
+    this.odometer= 0;
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons;
+    }
+  drive(distance) { 
+      this.odometer = this.odometer+distance;
+      this.tank = this.tank - (distance / this.milesPerGallon);
+       if( distance > this.tank * this.milesPerGallon){
+          this.odometer = this.odometer + (this.tank * this.milesPerGallon),
+          this.tank = 0;
+            }
+        return `I ran out of fuel at ${this.odometer}+ (${this.tank}*${this.milesPerGallon})`     
+       }  
 }
 
 /*
@@ -75,8 +105,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
-}
+  constructor(options){
+    this.name = options.name;
+    this.age = options.age;
+    this.location = options.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}.`
+  }
 
 /*
   TASK 4
@@ -92,8 +128,20 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(options){
+    super(options);
+    this.specialty = options.specialty;
+    this.favLanguage = options.favLanguage;
+    this.catchPhrase = options.catchPhrase;
+  };
 
+    demo(subject){
+      return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject){
+      return `${student.name} receives a perfect scrore on ${subject}`
+    }
 }
 
 /*
@@ -111,8 +159,22 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor(options){
+    super(options);
+    this.previousBackground = options.previousBackground;
+    this.className = options.className;
+    this.favSubjects = options.favSubjects;
+  }
+  listSubjects(){
+    return `Loving ${this.favSubjects}`;
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 }
 
 /*
@@ -128,10 +190,19 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor{
+  constructor(options){
+    super(options);
+    this.gradClassName = options.gradClassName;
+    this.favInstructor = options.favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`
+  }
+  debugsCode(object, subject){
+    return `${object.name} debugs ${this.name}` +`'s code on ${subject}`;
+  }
 }
-
 /*
   STRETCH PROBLEM (no tests!)
     - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
